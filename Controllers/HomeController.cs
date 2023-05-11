b7be94d3597417a5e4c1ad3de813c984bae5fc5a
Zamokuhle Mdunge <zamohzetmdunge@gmail.com>
2023-05-11 11:53:49 +0200

    Initial Project Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Foursquare.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using TDD_WebApp.Models;
+
+namespace TDD_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        string apiUrl = ConfigurationManager.AppSettings["APIUrl"].ToString();
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        // GET: Transact
+        public ActionResult Index()
+        {
+            string param = "Durban";
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            var response = client.GetAsync(apiUrl + "/api/Home/GetImageAsync"/* + param*/).Result;
+
+            string returnValue = response.Content.ReadAsStringAsync().Result;
+            string Premium = JsonConvert.DeserializeObject<string>(returnValue);
+            return Json(Premium, JsonRequestBehavior.AllowGet);
+            //return View();
+        }
+
+        // GET:Transactions/Details/5
+        public ActionResult GetLocatonsList(FoursquareLocationVM foursquareLocation)
+        {
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            var content = Newtonsoft.Json.JsonConvert.SerializeObject(foursquareLocation);
+            var response = client.PostAsync(apiUrl + "/api/Home/GetLoationDetailsAsync", new StringContent(content, Encoding.UTF8, "application/json")).Result;
+            string returnValue = response.Content.ReadAsStringAsync().Result;
+            string Premium = JsonConvert.DeserializeObject<string>(returnValue);
+            return Json(Premium, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ Models/FoursquareViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Foursquare.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDD_WebApp.Models
+{
+    public class FoursquareViewModel
+    {
+    }
+
+    public  class FoursquareLocationVM
+    {
+        
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public double Accuracy { get; set; }
+        public double Heading { get; set; }
+        public double Speed { get; set; }
+        public long Timestamp { get; set; }
+        public LocationSource Source { get; set; }
+
+        
+    }
+}
